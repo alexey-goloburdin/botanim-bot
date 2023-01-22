b08042ca9af89dec21a77e8aa2e1183b929f69d3
Олег Медовиков <57829409+oleg-medovikov@users.noreply.github.com>
2023-01-23 01:46:15 +0300

    Update num_to_words.py
    упростил немного

--- botanim_bot/num_to_words.py
+++ botanim_bot/num_to_words.py
@@ -1,14 +1,13 @@
 def books_to_words(books_count: int) -> str:
     books_count = books_count % 100
     last = books_count % 10
-    if 4 < last < 10 or last == 0 or 10 < books_count < 20:
-        return "книг"
-    if 1 < last < 5:
-        return "книги"
-    if last == 1:
-        return "книгу"
-    raise NotImplementedError
-
+    
+    return {
+        4 < last < 10 or last == 0 or 10 < books_count < 20: "книг",
+        1 < last < 5: "книги",
+        last == 1: "книгу"
+        }[True]
+    
 
 if __name__ == "__main__":
     for num in (1, 2, 5, 10, 11, 19, 20, 21, 121, 125):
