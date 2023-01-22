fb33f0f106dcbc7139c932602925910aeee764db
Gadzhi <62199271+Gadzhi07@users.noreply.github.com>
2023-01-22 23:01:03 +0300

    Теперь это работает для единицы

--- botanim_bot/numtowords.py
+++ botanim_bot/numtowords.py
@@ -1,10 +1,10 @@
 from num2words.lang_RU import Num2Word_RU
 
 def books2words(book_num: int) -> str:
-    num_to_words = Num2Word_RU._int2word(Num2Word_RU(), book_num, feminine=True) + " книг"
     if book_num == 1:
-        num_to_words += "а"
-    elif 1 < book_num < 5:
+        return "одну книгу"
+    num_to_words = Num2Word_RU._int2word(Num2Word_RU(), book_num, feminine=True) + " книг"
+    if 1 < book_num < 5:
         num_to_words += "и"
     return num_to_words
 
