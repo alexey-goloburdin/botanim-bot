0d10635c28296f026d88a8110bacb62c8dde47b6
Vladislav Atakhanov <eloh1mZee@gmail.com>
2023-01-23 04:32:26 +0600

    fix: fix 21+ books, and 1 book

--- botanim_bot/num_to_words.py
+++ botanim_bot/num_to_words.py
@@ -1,8 +1,10 @@
 def books_to_words(books_count: int) -> str:
-    match books_count:
-        case 1:
-            return "одна книга"
-        case 2 | 3 | 4:
-            return f"{books_count} книги"
-        case _:
-            return f"{books_count} книг"
+    books_count = books_count % 100
+    last = books_count % 10
+    if 4 < last < 10 or last == 0 or 10 < books_count < 20:
+        return "книг"
+    if 1 < last < 5:
+        return "книги"
+    if last == 1:
+        return "книгу"
+    raise NotImplementedError
