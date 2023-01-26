c9e5d9f6464eb455df8e54b19a9d61a2cea26143
Alexey Goloburdin <terrarium-a@yandex.ru>
2023-01-27 02:10:12 +0300

    docs: README updated

--- botanim_bot/config.py
+++ botanim_bot/config.py
@@ -1,13 +1,16 @@
 import os
+from pathlib import Path
 
 from dotenv import load_dotenv
 
 
 load_dotenv()
 
+
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
-SQLITE_DB_FILE = "db.sqlite3"
+BASE_DIR = Path(__file__).resolve().parent
+SQLITE_DB_FILE = BASE_DIR / "db.sqlite3"
 DATE_FORMAT = "%d.%m.%Y"
 VOTE_ELEMENTS_COUNT = 3
 
