2f23aad3581874e7f1ce372d1eb6878f7caa1cb7
nekrod <NP17210@yandex.ru>
2023-02-02 18:30:52 +0500

    fix

--- botanim_bot/__main__.py
+++ botanim_bot/__main__.py
@@ -12,6 +12,22 @@ from botanim_bot.db import close_db
 from botanim_bot import config
 from botanim_bot import handlers
 
+COMMAND_HANDLERS = {
+    "start": handlers.start,
+    "help": handlers.help_,
+    "allbooks": handlers.all_books,
+    "already": handlers.already,
+    "now": handlers.now,
+    "vote": handlers.vote,
+    "cancel": handlers.cancel,
+    "voteresults": handlers.vote_results,
+}
+
+CALLBACK_QUERY_HANDLERS = {
+    rf"^{config.ALL_BOOKS_CALLBACK_PATTERN}(\d+)$": handlers.all_books_button,
+    rf"^{config.VOTE_BOOKS_CALLBACK_PATTERN}(\d+)$": handlers.vote_button,
+}
+
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
@@ -26,23 +42,9 @@ if not config.TELEGRAM_BOT_TOKEN or not config.TELEGRAM_BOTANIM_CHANNEL_ID:
 def main():
     application = ApplicationBuilder().token(config.TELEGRAM_BOT_TOKEN).build()
 
-    COMMAND_HANDLERS = {
-        "start": handlers.start,
-        "help": handlers.help_,
-        "allbooks": handlers.all_books,
-        "already": handlers.already,
-        "now": handlers.now,
-        "vote": handlers.vote,
-        "cancel": handlers.cancel,
-        "voteresults": handlers.vote_results,
-    }
     for command_name, command_handler in COMMAND_HANDLERS.items():
         application.add_handler(CommandHandler(command_name, command_handler))
 
-    CALLBACK_QUERY_HANDLERS = {
-        rf"^{config.ALL_BOOKS_CALLBACK_PATTERN}(\d+)$": handlers.all_books_button,
-        rf"^{config.VOTE_BOOKS_CALLBACK_PATTERN}(\d+)$": handlers.vote_button,
-    }
     for pattern, handler in CALLBACK_QUERY_HANDLERS.items():
         application.add_handler(CallbackQueryHandler(handler, pattern=pattern))
 
