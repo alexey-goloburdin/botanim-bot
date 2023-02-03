0c958a509949a9eeb914edc7b7bbce48d2e98dc2
nekrod <NP17210@yandex.ru>
2023-02-03 22:29:57 +0500

    fix errors

--- botanim_bot/templates.py
+++ botanim_bot/templates.py
@@ -20,12 +20,13 @@ def render_template(template_name: str, data: dict | None = None) -> str:
 def _get_template_env():
     if not getattr(_get_template_env, "template_env", None):
         template_loader = jinja2.FileSystemLoader(searchpath=config.TEMPLATES_DIR)
-        env = jinja2.Environment(
-            loader=template_loader, trim_blocks=True, lstrip_blocks=True
-        )
-        setattr(
-            _get_template_env,
-            "template_env",
-            env,
-        )
+        env = jinja2.Environment(loader=template_loader,
+                                 trim_blocks=True,
+                                 lstrip_blocks=True,
+                                 autoescape=True)
+
+        setattr(_get_template_env,
+                "template_env",
+                env)
+
     return getattr(_get_template_env, "template_env")
