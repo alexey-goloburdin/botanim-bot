9dda19757d1edb67c13530929825f23dc8497cf1
nekrod <NP17210@yandex.ru>
2023-02-04 22:13:30 +0500

    refactor

--- botanim_bot/services/num_to_words.py
+++ botanim_bot/services/num_to_words.py
@@ -1,9 +1,7 @@
 def num_to_words(count: int, word_forms: tuple[str, str, str]) -> str:
-    if 11 <= count % 100 <= 19:
-        p = 2
-    elif count % 10 == 1:
+    if count % 10 == 1 and count % 100 != 11:
         p = 0
-    elif 2 <= count % 10 <= 4:
+    elif 2 <= count % 10 <= 4 and (count % 100 < 10 or count % 100 >= 20):
         p = 1
     else:
         p = 2
