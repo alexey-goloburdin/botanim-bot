0e5a958c2f0e9f605a804e7f7541611875e1569b
nekrod <NP17210@yandex.ru>
2023-02-04 21:41:24 +0500

    refactor

--- botanim_bot/services/num_to_words.py
+++ botanim_bot/services/num_to_words.py
@@ -2,24 +2,13 @@ DAYS = ("книга", "книги", "книг")
 
 
 def books_to_words(books_count: int) -> str:
-    penultimate_digit, last_digit = _get_last_two_digits(books_count)
-
-    if penultimate_digit == 1:
-        index = 2
+    if 11 <= books_count % 100 <= 19:
+        p = 2
+    elif books_count % 10 == 1:
+        p = 0
+    elif 2 <= books_count % 10 <= 4:
+        p = 1
     else:
-        if last_digit == 1:
-            index = 0
-        elif last_digit in (2, 3, 4):
-            index = 1
-        else:
-            index = 2
+        p = 2
 
-    return DAYS[index]
-
-
-def _get_last_two_digits(number: int) -> tuple[int, int]:
-    list_of_digits = list(str(number))
-    if len(list_of_digits) == 1:
-        return 0, number
-
-    return int(list_of_digits[-2]), int(list_of_digits[-1])
+    return DAYS[p]
